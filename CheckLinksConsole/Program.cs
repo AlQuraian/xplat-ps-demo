c34c21431143001d8de4b33ebf7dd4a08fe6a897
Mohammad AlQuraian <m4design@gmail.com>
2018-08-21 18:58:33 +0300

    Add configuration

--- CheckLinksConsole/Program.cs
+++ CheckLinksConsole/Program.cs
@@ -1,34 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace CheckLinksConsole
 {
+    class OutputSettings
+    {
+        public string Folder { get; set; }
+        public string File { get; set; }
+        public string ReportFilePath => Path.Combine(Directory.GetCurrentDirectory(), Folder, File);
+        public string ReportDirectory => Path.GetDirectoryName(ReportFilePath);
+    }
+
     class Program
     {
         static async Task Main(string[] args)
         {
-            var site = "https://g0t4.github.io/pluralsight-dotnet-core-xplat-apps";
+            var inMemory = new Dictionary<string, string>
+            {
+                {"site", "https://google.com"}
+            };
+
+            var configBuilder = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemory)
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("checksettings.json", true)
+                .AddCommandLine(args)
+                .AddEnvironmentVariables();
+            var configuration = configBuilder.Build();
+
+            var site = configuration["site"];
             var body = await new HttpClient().GetStringAsync(site);
-            System.Console.WriteLine(body);
 
             System.Console.WriteLine();
             System.Console.WriteLine("Links");
             var links = LinkChecker.GetLinks(body);
             links.ToList().ForEach(System.Console.WriteLine);
 
-            var currentDirectory = Directory.GetCurrentDirectory();
-            var outputFolder = "reports";
-            var outputFile = "report.txt";
-            var outputPath = Path.Combine(currentDirectory, outputFolder, outputFile);
-            var directory = Path.GetDirectoryName(outputPath);
-            Directory.CreateDirectory(directory);
-            // File.WriteAllLines(outputPath, links);
+            var output = configuration.GetSection("output").Get<OutputSettings>();
+            Directory.CreateDirectory(output.ReportDirectory);
 
             var checkedLinks = await LinkChecker.Check(links);
-            using (var file = File.CreateText(outputPath))
+            using (var file = File.CreateText(output.ReportFilePath))
             {
                 foreach (var link in checkedLinks.OrderBy(l => l.Exists))
                 {
